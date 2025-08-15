f85ed61acf93f334a2bff4e2062ae3685a9c6c97
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-15 23:28:46 +0000

    commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * @file main.js
+ * This is the primary entry point for the Adaptive Training Companion application.
+ * It is responsible for loading the main application component and attaching it to the webpage.
+ */
+
+// We are importing the main app component, which we will create later.
+// This line tells our app to load the code for the main user interface.
+import "./components/app-shell.js";
+
+console.log("Adaptive Training Companion initialized!");
