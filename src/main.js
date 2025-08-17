a25d4cd230a97ef2981cf8a39780afdf6c8053d1
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-16 23:21:34 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@
 // We are importing the main app component, which we will create later.
 // This line tells our app to load the code for the main user interface.
 import "./components/app-shell.js";
+// Import the new syncData function from the API service
+import { syncData } from "./services/api.js";
 // Remove this line: import "./style.css";
 
 console.log("Adaptive Training Companion initialized!");
+
+// Call the new syncData function to handle any queued offline data
+syncData();
