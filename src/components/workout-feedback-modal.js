4f9306a8548570bfad8a49d0b0d68cc30e889557
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-17 13:55:00 -0500

    Update workout-feedback-modal.js

--- src/components/workout-feedback-modal.js
+++ src/components/workout-feedback-modal.js
@@ -58,14 +58,16 @@ class WorkoutFeedbackModal extends LitElement {
 
     return html`
       <div class="modal-overlay" @click=${this._handleOverlayClick}>
-        <div class="glass-card modal-content" 
+        <div class="card modal-content" 
              role="dialog" 
              aria-modal="true" 
              aria-labelledby="modal-title"
              @keydown=${this._handleKeydown}>
           <div class="modal-header">
             <h3 id="modal-title" class="modal-title">How was that set?</h3>
-            <button class="close-button" @click=${this._handleClose} aria-label="Close feedback modal">×</button>
+            <button class="close-button" @click=${this._handleClose} aria-label="Close feedback modal">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+            </button>
           </div>
 
           ${questions.map(question => html`
