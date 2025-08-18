b3fde3e0986733eab2136f960784a41ccc605d40
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-18 07:13:20 -0500

    Update workout-feedback-modal.js

--- src/components/workout-feedback-modal.js
+++ src/components/workout-feedback-modal.js
@@ -64,7 +64,7 @@ class WorkoutFeedbackModal extends LitElement {
              aria-labelledby="modal-title"
              @keydown=${this._handleKeydown}>
           <div class="modal-header">
-            <h3 id="modal-title" class="modal-title">How was that set?</h3>
+            <h3 id="modal-title" class="modal-title">Set Feedback</h3>
             <button class="close-button" @click=${this._handleClose} aria-label="Close feedback modal">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
             </button>
