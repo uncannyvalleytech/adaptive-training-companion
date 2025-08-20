bf49dc5d038c1e38414c4484eab5fb03c9d5df4b
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-20 17:48:24 -0500

    Update settings-view.js

--- src/components/settings-view.js
+++ src/components/settings-view.js
@@ -61,13 +61,6 @@ class SettingsView extends LitElement {
   render() {
     return html`
       <div class="settings-container container">
-        <header class="app-header">
-            <button class="btn btn-icon" @click=${() => this._dispatchEvent('setView', { view: 'home' })} aria-label="Back to Home">
-             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5"/><path d="m12 19-7-7 7-7"/></svg>
-          </button>
-          <h1>Settings</h1>
-        </header>
-
         <div class="card settings-group">
           <h3>Appearance</h3>
           <div class="setting-item">
