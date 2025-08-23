e1e7768079716c2f72806716e2eac686610602c7
uncannyvalleytech <uncannyvalleytech@gmail.com>
2025-08-23 06:38:31 -0500

    Update routine-selector.js

--- src/components/routine-selector.js
+++ src/components/routine-selector.js
@@ -11,7 +11,11 @@ class RoutineSelector extends LitElement {
     return html`
       <div class="container routine-selector">
         <header class="app-header">
+          <button class="btn btn-icon" @click=${() => this.dispatchEvent(new CustomEvent('setView', { detail: { view: 'home' }, bubbles: true, composed: true }))} aria-label="Back">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5"/><path d="m12 19-7-7 7-7"/></svg>
+          </button>
           <h1>New Routine</h1>
+          <div style="width: 48px;"></div>
         </header>
 
         <div class="options-list">
